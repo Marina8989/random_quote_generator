c78711c1163b39e36a5d400248a5998e4526c036
Marina8989 <marina.mill209@gmail.com>
2020-06-21 22:13:46 -0700

    added citation and year properties

--- js/script.js
+++ js/script.js
@@ -5,23 +5,33 @@
 const quotes = [
     {
       quote: 'one',
-      source: 'one'
+      source: 'one',
+      citation: 'book',
+      year: '2020'
     },
     {
       quote: 'two',
-      source: 'two'
+      source: 'two',
+      citation: 'movie',
+      year: '2019'
     },
     {
       quote: 'three',
-      source: 'three'
+      source: 'three',
+      citation: 'actor',
+      year: '2018'
     },
     {
       quote: 'four',
-      source: 'four'
+      source: 'four',
+      citation: 'polititian',
+      year: '2017'
     },
     {
       quote: 'five',
-      source: 'five'
+      source: 'five',
+      citation: 'mom',
+      year: '2016'
     }
 ];
 
