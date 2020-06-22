40647c7ae186e0b362d7a64eaefcd250e455c7e2
Marina8989 <marina.mill209@gmail.com>
2020-06-22 14:34:30 -0700

    finished the project, now working on extras

--- js/script.js
+++ js/script.js
@@ -3,66 +3,81 @@
  * `quotes` array 
 ***/
 const quotes = [
-    {
-      quote: 'one',
-      source: 'one',
-      citation: 'book',
-      year: '2020'
-    },
-    {
-      quote: 'two',
-      source: 'two',
-      citation: 'movie',
-      year: '2019'
-    },
-    {
-      quote: 'three',
-      source: 'three',
-      citation: 'actor',
-      
-    },
-    {
-      quote: 'four',
-      source: 'four',
-      citation: 'polititian',
-      year: '2017'
-    },
-    {
-      quote: 'five',
-      source: 'five',
-      
-    }
+  {
+    quote: "Anything that is of value in life only multiplies when it is given.",
+    source: "Deepak Chopra",
+    citation: "Wikipedia",
+    tags: "Motivation"
+  },
+  {
+    quote: "Nothing will work unless you do.",
+    source: "Maya Angelou",
+    year: "1928-2014"
+  },
+  {
+    quote: "A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing.",
+    source: "George Bernhard Shaw",
+    citation: "Twitter",
+    year: "1856-1950",
+  },
+  {
+    quote: "The two most powerful warriors are patience and time.",
+    source: "Leo Tolstoy",
+    citation: "Russian Writer",
+  },
+  {
+    quote: "The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.",
+    source: "Albert Eistein",
+    year: '1879-1955',
+    tags: "Education"
+  },
 ];
 
 
 /***
  * `getRandomQuote` function
 ***/
-  function getRandomQuote() {
-    let randomNumber = Math.floor(Math.random() * quotes.length);
-     return quotes[randomNumber];
+  function getRandomQuote(array) {
+    let randomNumber = Math.floor(Math.random() * array.length);
+     return array[randomNumber];
   }
-
-
+   
 /***
  * `printQuote` function
 ***/
   function printQuote() {
-    let printQ = getRandomQuote();
+    let printRandomQuote = getRandomQuote(quotes);
+    
     let htmlString = 
-    `<p class='quote'>${printQ.quote}</p>
-     <p class='source'>${printQ.source}
-    
-      if(typeof ${printQ.citation} != 'undefined') {
-        <span class='citation'>${printQ.citation}</span>
+    `
+    <p class='quote'>${printRandomQuote.quote}</p>
+    <p class='source'>${printRandomQuote.source}`
+      if(printRandomQuote.hasOwnProperty('citation')) {
+      htmlString+= `<span class='citation'>${printRandomQuote.citation}</span>`
       }
-    
+      if (printRandomQuote.hasOwnProperty('year')) {
+        htmlString += `<span class='year'>${printRandomQuote.year}</span>`;
+      }
+      if (printRandomQuote.hasOwnProperty('tags')) {
+        htmlString += `<span class='year'>${printRandomQuote.tags}</span>`;
+      }
+    `</p>
+    `;
 
-    </p >`;
-    return document.getElementById('quote-box').innerHTML = htmlString; 
-   
+    return (document.getElementById("quote-box").innerHTML = htmlString);
   }
 
+    function randomColor() {
+      let color = Math.floor(Math.random() * 256);
+        let red = 
+
+    }
+   
+
+
+    setInterval(() => {
+        printQuote();
+    }, 10000)
 
 /***
  * click event listener for the print quote button
